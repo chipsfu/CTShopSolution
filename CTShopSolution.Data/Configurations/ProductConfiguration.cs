afdc5dca081d5525af4975802f053e5284b5bdb1
YEN_HA <chipsfu@gmail.com>
2020-08-01 13:21:54 +0700

    Create database design using Fluent API Configuration

---
+++ CTShopSolution.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CTShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CTShopSolution.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            //cau hinh ten bang, thuoc tinh
+            builder.ToTable("Products");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Price).IsRequired();
+            builder.Property(x => x.OriginalPrice).IsRequired();
+            builder.Property(x => x.Stock).IsRequired().HasDefaultValue(0);
+            builder.Property(x => x.ViewCount).IsRequired().HasDefaultValue(0);
+        }
+    }
+}
