4e8a78ecf74c354109cdbb8e3d14de7b140b0a35
YEN_HA <chipsfu@gmail.com>
2020-08-01 09:12:05 +0700

    Add project files.

---
+++ CTShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CTShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
